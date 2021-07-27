a4b01bd65545496f96736e2f2d7a49ee93f438ba
drgomesp <drgomesp@gmail.com>
2021-07-27 19:12:54 -0300

    added name method to server interface

--- pkg/node/server.go
+++ pkg/node/server.go
@@ -4,6 +4,7 @@ import "context"
 
 // Server defines a server that can be registered to the node.
 type Server interface {
+	Name() string
 	Start(ctx context.Context) error
 	Stop(ctx context.Context) error
 }
