b3e24053f7f1cb0c7c8d43ed3e82eddd3c34b228
drgomesp <drgomesp@gmail.com>
2021-07-28 15:17:11 -0300

    added comments to server interface

--- pkg/node/server.go
+++ pkg/node/server.go
@@ -4,7 +4,12 @@ import "context"
 
 // Server defines a server that can be registered to the node.
 type Server interface {
+	// Name of the server.
 	Name() string
+
+	// Start the server.
 	Start(ctx context.Context) error
+
+	// Stop the server.
 	Stop(ctx context.Context) error
 }
